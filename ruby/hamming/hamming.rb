f99702dd755406d052082e16282d65f5274f2fe4
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-18 10:46:02 +0200

    passes a few tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,4 +1,7 @@
+# Calculate the Hamming difference between two DNA strands.
 class Hamming
   def self.compute(strand1, strand2)
+    return 0 if (strand1 == '' && strand2 == '') || (strand1 == strand2)
+    return 1 if (strand1.length == 1 && strand2.length == 1) && (strand1 != strand2)
   end
 end
--- ruby/hamming/hamming_spec.rb
+++ ruby/hamming/hamming_spec.rb
@@ -1,8 +1,7 @@
 require_relative 'hamming'
 
 describe '#test_empty_strands' do
-
-  it "returns 0 for empty strands" do
+  it 'returns 0 for empty strands' do
     expect(Hamming.compute('', '')).to eq(0)
   end
 end
