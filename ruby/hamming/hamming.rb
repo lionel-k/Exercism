3110aab0b7614ef4d6c906644968e19cb456ec30
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-19 11:47:24 +0200

    1st solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,20 @@
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
 # Calculate the Hamming difference between two DNA strands.
 class Hamming
   def self.compute(strand1, strand2)
-    return 0 if (strand1 == '' && strand2 == '') || (strand1 == strand2)
-    return 1 if (strand1.length == 1 && strand2.length == 1) && (strand1 != strand2)
+    # if (strand1 == '' && strand2 == '') || (strand1 == strand2)
+    #   0
+    # elsif (strand1.length == 1 && strand2.length == 1) && (strand1 != strand2)
+    #   1
+    # else
+    #   2
+    # end
+    diff = 0
+    strand1.length.times do |i|
+      diff += 1 if strand1[i] != strand2[i]
+    end
+    diff
   end
 end
