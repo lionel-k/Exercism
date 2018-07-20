8a529ddb2dd59947af8b224bf5b4e0f8af76410f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-20 10:14:23 +0200

    1st solution

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,10 @@
+# Calculate the moment when someone has lived for 109 seconds.
+class Gigasecond
+  def self.from(time)
+    time + 1_000_000_000.0
+  end
+end
+
+module BookKeeping
+  VERSION = 6
+end
