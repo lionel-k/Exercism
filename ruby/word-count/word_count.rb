bd7f6064f89f759e592069716ba956530a5d759b
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-20 18:16:57 +0200

    regex in a constant

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,7 +1,8 @@
 # Given a phrase, count the occurrences of each word in that phrase.
 class Phrase
+  WORD_REGEX = '\w+\'*+\w+|\d'.freeze
   def initialize(phrase)
-    @words = phrase.downcase.scan(/\w+'*+\w+|\d/)
+    @words = phrase.downcase.scan(/#{WORD_REGEX}/)
   end
 
   def word_count
