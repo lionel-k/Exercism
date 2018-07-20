522d0791b27d5e360c7a993f84e4421bd77a57a2
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-20 15:24:28 +0200

    all tests pass

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,11 +1,11 @@
 # Given a DNA strand, return its RNA complement (per RNA transcription).
 class Complement
   def self.of_dna(strand)
-    strand
+    return '' unless strand.gsub(/[GCTA]/, '') == ''
+    strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
   end
 end
 
 module BookKeeping
   VERSION = 4
 end
-
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -4,42 +4,42 @@ require_relative 'rna_transcription'
 # Common test data version: 1.0.1 cb1fd3a
 class RnaTranscriptionTest < Minitest::Test
   def test_rna_complement_of_cytosine_is_guanine
-    # skip
+    # # skip
     assert_equal 'G', Complement.of_dna('C')
   end
 
   def test_rna_complement_of_guanine_is_cytosine
-    skip
+    # skip
     assert_equal 'C', Complement.of_dna('G')
   end
 
   def test_rna_complement_of_thymine_is_adenine
-    skip
+    # skip
     assert_equal 'A', Complement.of_dna('T')
   end
 
   def test_rna_complement_of_adenine_is_uracil
-    skip
+    # skip
     assert_equal 'U', Complement.of_dna('A')
   end
 
   def test_rna_complement
-    skip
+    # skip
     assert_equal 'UGCACCAGAAUU', Complement.of_dna('ACGTGGTCTTAA')
   end
 
   def test_correctly_handles_invalid_input_rna_instead_of_dna
-    skip
+    # skip
     assert_equal '', Complement.of_dna('U')
   end
 
   def test_correctly_handles_completely_invalid_dna_input
-    skip
+    # skip
     assert_equal '', Complement.of_dna('XXX')
   end
 
   def test_correctly_handles_partially_invalid_dna_input
-    skip
+    # skip
     assert_equal '', Complement.of_dna('ACGTXXXCTTAA')
   end
 
@@ -61,7 +61,7 @@ class RnaTranscriptionTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 4, BookKeeping::VERSION
   end
 end
