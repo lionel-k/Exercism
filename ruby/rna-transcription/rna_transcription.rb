35c68dcb8556884779c5a324ae39882467f3b362
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-20 15:12:25 +0200

    dowload exce

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,11 @@
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+class Complement
+  def self.of_dna(strand)
+    strand
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
+
