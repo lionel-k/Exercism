c140c392162f6c2a4d2b3912f65ab00f32c52c62
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-23 09:54:09 +0200

    refactor private class method

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,21 +2,24 @@
 # given that the number on each square doubles.
 class Grains
   CHESS_SIZE = 64
-  def self.square(square_number)
-    if non_existing_square_number(square_number)
-      raise ArgumentError, 'Square number should be included between 0 and 64'
+
+  class << self
+    def square(square_number)
+      if non_existing_square_number(square_number)
+        raise ArgumentError, 'Square number should be included between 0 and 64'
+      end
+      (2**square_number) / 2
     end
-    (2**square_number) / 2
-  end
 
-  def self.total
-    1.upto(CHESS_SIZE).inject { |sum, n| sum + square(n) }
-  end
+    def total
+      1.upto(CHESS_SIZE).inject { |sum, n| sum + square(n) }
+    end
 
-  private_class_method
+    private
 
-  def self.non_existing_square_number(square_number)
-    square_number <= 0 || square_number > CHESS_SIZE
+    def non_existing_square_number(square_number)
+      square_number <= 0 || square_number > CHESS_SIZE
+    end
   end
 end
 
