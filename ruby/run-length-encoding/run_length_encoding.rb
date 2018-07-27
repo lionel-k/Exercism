b8d521baf14413f266780221cb9e0756e205ce6f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-27 10:58:09 +0200

    better solution encode

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -3,24 +3,45 @@ class RunLengthEncoding
   def self.encode(input)
     return '' if input.empty?
     initials = []
-    counts = []
-    current = input[0]
-    initials << current
-    i = 0
-    counts[i] = 0
-    input.each_char do |chr|
-      if chr == current
-        counts[i] += 1
-      else
-        i += 1
-        counts[i] = 1
-        current = chr
-        initials << current
-      end
-    end
+    # counts = []
+    # current = input[0]
+    # initials << current
+    # i = 0
+    # counts[i] = 0
+    # input.each_char do |chr|
+    #   if chr == current
+    #     counts[i] += 1
+    #   else
+    #     i += 1
+    #     counts[i] = 1
+    #     current = chr
+    #     initials << current
+    #   end
+    # end
 
-    initials.each_with_index.map do |chr, index|
-      (counts[index] == 1 ? chr : "#{counts[index]}#{chr}")
+    # initials.each_with_index.map do |chr, index|
+    #   (counts[index] == 1 ? chr : "#{counts[index]}#{chr}")
+    # end.join
+    current_index = 0
+    initials[0] = ""
+    input.split("").each_with_index do |chr, index|
+      # puts "#{chr} - #{current}"
+      if chr == input[current_index]
+        # puts "same value"
+        initials[-1] << chr
+        # puts input[current_index]
+      else
+        # puts "new value"
+        initials.push(chr)
+        current_index = index
+      end
+      # print initials
+      # puts ""
+      # puts "#{chr} - #{current}"
+    end
+    initials.map do |substring|
+      substring_size = substring.size
+      substring_size == 1 ? substring : "#{substring_size}#{substring[0]}"
     end.join
   end
 
@@ -46,8 +67,8 @@ input = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB'
 # input = '  hsqq qww  '
 # output = '12WB12W3B24WB'
 # input = ''
+# print RunLengthEncoding.encode(input)
 print RunLengthEncoding.encode(input)
-# RunLengthEncoding.encode(input)
 
 # input = 'XYZ'
 # input = '2A3B4C'
