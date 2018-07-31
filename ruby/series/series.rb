f60c5b5fc6fcd4266c01b114aac35db3079234d1
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-31 10:52:56 +0200

    first solution

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,10 +1,22 @@
-# Given a string of digits, output all the contiguous substrings of length n in that string.
+# Given a string of digits,
+# output all the contiguous substrings of length n in that string.
 class Series
-  def initialize
+  ERROR_MESSAGE = 'The size should be <= of the length of the digits.'.freeze
 
+  def initialize(digits)
+    @digits = digits
+    @length = digits.size
   end
 
-  def slices
-
+  def slices(size)
+    raise ArgumentError, ERROR_MESSAGE if size > @length
+    0.upto(@length - size).map do |i|
+      @digits[i..-1].scan(/.{#{size}}/).first
+    end
   end
 end
+
+# series = Series.new('98273463')
+# series = Series.new('49142')
+# expected = ['98', '82', '27', '73', '34', '46', '63']
+# print series.slices(6)
