8248c96044623d198e769cf00f8659e94f2c9198
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-08-08 13:56:14 +0200

    first solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,19 @@
 # Given a year, report if it is a leap year.
 class Year
-  def leap?(year)
+  def self.leap?(year)
+    (divisible_by_4?(year) && (not divisible_by_100?(year))) || (divisible_by_4?(year) && divisible_by_100?(year) && divisible_by_400?(year)) || (divisible_by_400?(year))
+  end
 
+  def self.divisible_by_4?(year)
+    (year % 4).zero?
+  end
+
+  def self.divisible_by_100?(year)
+    (year % 100).zero?
+  end
+
+  def self.divisible_by_400?(year)
+    (year % 400).zero?
   end
 end
 
