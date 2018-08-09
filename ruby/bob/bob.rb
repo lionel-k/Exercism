e7da9fb8fb4ddfc041f316654fc0d58777a83915
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-08-09 10:28:57 +0200

    refactor witl class << self

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -7,29 +7,30 @@
 # if you address him without actually saying anything.
 # He answers 'Whatever.' to anything else.
 class Bob
-  def self.hey(remark)
-    return 'Calm down, I know what I\'m doing!' if being_questionned_by(remark) && being_yelled_at_by(remark)
-    return 'Sure.' if being_questionned_by(remark)
-    # return 'Whoa, chill out!' if remark.upcase == remark || remark[-1] == '!'
-    return 'Whoa, chill out!' if being_yelled_at_by(remark)
-    return 'Fine. Be that way!' if being_said_nothing_by(remark)
-    'Whatever.'
-  end
+  class << self
+    def hey(remark)
+      if being_questionned_by(remark) && being_yelled_at_by(remark)
+        return 'Calm down, I know what I\'m doing!'
+      end
+      return 'Sure.' if being_questionned_by(remark)
+      return 'Whoa, chill out!' if being_yelled_at_by(remark)
+      return 'Fine. Be that way!' if being_said_nothing_by(remark)
+      'Whatever.'
+    end
 
-  # private
+    private
 
-  def self.being_questionned_by(remark)
-    remark.strip[-1] == '?'
-  end
+    def being_questionned_by(remark)
+      remark.strip[-1] == '?'
+    end
 
-  def self.being_yelled_at_by(remark)
-    # remark.gsub
-    # remark.gsub!(/\d/, "")
-    remark.upcase == remark && remark.count("a-dA-Z") > 0
-  end
+    def being_yelled_at_by(remark)
+      remark.upcase == remark && remark.count('a-dA-Z') > 0
+    end
 
-  def self.being_said_nothing_by(remark)
-    remark.strip == ''
+    def being_said_nothing_by(remark)
+      remark.strip == ''
+    end
   end
 end
 
