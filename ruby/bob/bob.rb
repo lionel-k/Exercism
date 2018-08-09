39ab1b69c440c0dd079f0fa563a14148b89912eb
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-08-09 10:24:13 +0200

    first solution

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -4,7 +4,7 @@
 # He answers 'Whoa, chill out!' if you yell at him.
 # He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
 # He says 'Fine. Be that way!'
-# rif you address him without actually saying anything.
+# if you address him without actually saying anything.
 # He answers 'Whatever.' to anything else.
 class Bob
   def self.hey(remark)
@@ -12,23 +12,24 @@ class Bob
     return 'Sure.' if being_questionned_by(remark)
     # return 'Whoa, chill out!' if remark.upcase == remark || remark[-1] == '!'
     return 'Whoa, chill out!' if being_yelled_at_by(remark)
+    return 'Fine. Be that way!' if being_said_nothing_by(remark)
     'Whatever.'
   end
 
   # private
 
   def self.being_questionned_by(remark)
-    remark[-1] == '?'
+    remark.strip[-1] == '?'
   end
 
   def self.being_yelled_at_by(remark)
     # remark.gsub
     # remark.gsub!(/\d/, "")
-    remark.upcase == remark
+    remark.upcase == remark && remark.count("a-dA-Z") > 0
   end
 
   def self.being_said_nothing_by(remark)
-    remark == ""
+    remark.strip == ''
   end
 end
 
--- ruby/bob/bob_test.rb
+++ ruby/bob/bob_test.rb
@@ -64,13 +64,13 @@ class BobTest < Minitest::Test
   end
 
   def test_only_numbers
-    skip
+    # skip
     remark = "1, 2, 3"
     assert_equal "Whatever.", Bob.hey(remark), %q{Bob hears "1, 2, 3", and..}
   end
 
   def test_question_with_only_numbers
-    skip
+    # skip
     remark = "4?"
     assert_equal "Sure.", Bob.hey(remark), %q{Bob hears "4?", and..}
   end
@@ -94,7 +94,7 @@ class BobTest < Minitest::Test
   end
 
   def test_non_letters_with_question
-    skip
+    # skip
     remark = ":) ?"
     assert_equal "Sure.", Bob.hey(remark), %q{Bob hears ":) ?", and..}
   end
@@ -112,43 +112,43 @@ class BobTest < Minitest::Test
   end
 
   def test_prolonged_silence
-    skip
+    # skip
     remark = "          "
     assert_equal "Fine. Be that way!", Bob.hey(remark), %q{Bob hears "          ", and..}
   end
 
   def test_alternate_silence
-    skip
+    # skip
     remark = "\t\t\t\t\t\t\t\t\t\t"
     assert_equal "Fine. Be that way!", Bob.hey(remark), %q{Bob hears "\t\t\t\t\t\t\t\t\t\t", and..}
   end
 
   def test_multiple_line_question
-    skip
+    # skip
     remark = "\nDoes this cryogenic chamber make me look fat?\nno"
     assert_equal "Whatever.", Bob.hey(remark), %q{Bob hears "\nDoes this cryogenic chamber make me look fat?\nno", and..}
   end
 
   def test_starting_with_whitespace
-    skip
+    # skip
     remark = "         hmmmmmmm..."
     assert_equal "Whatever.", Bob.hey(remark), %q{Bob hears "         hmmmmmmm...", and..}
   end
 
   def test_ending_with_whitespace
-    skip
+    # skip
     remark = "Okay if like my  spacebar  quite a bit?   "
     assert_equal "Sure.", Bob.hey(remark), %q{Bob hears "Okay if like my  spacebar  quite a bit?   ", and..}
   end
 
   def test_other_whitespace
-    skip
+    # skip
     remark = "\n\r \t"
     assert_equal "Fine. Be that way!", Bob.hey(remark), %q{Bob hears "\n\r \t", and..}
   end
 
   def test_non_question_ending_with_whitespace
-    skip
+    # skip
     remark = "This is a statement ending with whitespace      "
     assert_equal "Whatever.", Bob.hey(remark), %q{Bob hears "This is a statement ending with whitespace      ", and..}
   end
@@ -171,7 +171,7 @@ class BobTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
