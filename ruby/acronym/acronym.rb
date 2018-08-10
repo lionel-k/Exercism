a7ed26054c3629ed4bf9c2e32c309a42d2f5dc2a
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-08-10 16:00:40 +0200

    first solution

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,3 +1,10 @@
 # Convert a phrase to its acronym.
 class Acronym
+  def self.abbreviate(phrase)
+    phrase.upcase.scan(/\w+/).map { |word| word[0] }.join
+  end
+end
+
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
 end
